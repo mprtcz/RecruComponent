0925f83d8133dbb6cc918b449c003deb8a5d6ff7
mprtcz <azet89"gmail.com>
2019-05-25 20:35:39 +0200

    Added spring boot skeleton

---
+++ src/test/java/com/mprtcz/recrucomponent/TheComponent/TheComponentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mprtcz.recrucomponent.TheComponent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TheComponentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
